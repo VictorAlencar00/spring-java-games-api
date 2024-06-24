a6518105c01a6bd257ee1aebca4bb19e6c9326ea
VictorAlencar00 <victordesousaa@gmail.com>
2024-06-24 20:04:13 -0300

    feat: spring boot initial commit

---
+++ src/test/java/com/victor_alencar/games/api_games/ApiGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor_alencar.games.api_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
